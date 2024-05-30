1f304db60a075696ad9de81ef12690cd7975708f
CVillardo04 <128702918+CVillardo04@users.noreply.github.com>
2024-05-30 17:44:29 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import atm_abi from "../artifacts/contracts/Chester.sol/Assessment.json";
+import atm_abi from "../artifacts/contracts/Chester.sol/Chester.json";
 
 export default function HomePage() {
   const [ethWallet, setEthWallet] = useState(undefined);
