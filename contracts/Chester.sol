6643f22b58e35c5ade42040c03b2ab0e6139445e
CVillardo04 <128702918+CVillardo04@users.noreply.github.com>
2024-06-02 03:55:49 +0800

    Update Chester.sol

--- contracts/Chester.sol
+++ contracts/Chester.sol
@@ -7,6 +7,9 @@ contract Chester {
 
     event Deposit(address indexed user, uint256 amount);
     event Withdraw(address indexed user, uint256 amount);
+    event ContractBalance(uint256 balance);
+    event WithdrawAll(address indexed owner, uint256 amount);
+    event DestroyContract(address indexed owner, uint256 balanceBeforeDestruction);
 
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
     error NotOwner();
@@ -20,6 +23,7 @@ contract Chester {
 
     constructor() payable {
         owner = payable(msg.sender);
+        emit ContractBalance(address(this).balance);
     }
 
     function getBalance() public view returns (uint256) {
@@ -29,6 +33,7 @@ contract Chester {
     function deposit() public payable {
         userBalances[msg.sender] += msg.value;
         emit Deposit(msg.sender, msg.value);
+        emit ContractBalance(address(this).balance);
     }
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -38,15 +43,22 @@ contract Chester {
         userBalances[msg.sender] -= _withdrawAmount;
         payable(msg.sender).transfer(_withdrawAmount);
         emit Withdraw(msg.sender, _withdrawAmount);
+        emit ContractBalance(address(this).balance);
     }
 
     function withdrawAll() public onlyOwner {
         uint256 contractBalance = address(this).balance;
         payable(owner).transfer(contractBalance);
+        emit WithdrawAll(owner, contractBalance);
+        emit ContractBalance(0);  // Balance will be zero after withdrawing all funds
     }
 
     function destroyContract() public onlyOwner {
+        uint256 contractBalance = address(this).balance;
+        emit DestroyContract(owner, contractBalance);
+        emit ContractBalance(0);  // Balance will be zero after contract destruction
         selfdestruct(owner);
     }
 }
+
 
